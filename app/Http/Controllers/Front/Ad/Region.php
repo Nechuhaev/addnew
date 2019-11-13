9adbe9804f45a86ab6953ffe5932ecc3a47601d9
vagrant <vagrant@homestead>
2019-11-13 23:06:50 +0000

    Добавлена страница для регионов

--- app/Http/Controllers/Front/Ad/Region.php
+++ app/Http/Controllers/Front/Ad/Region.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\Front\Ad;
 
+use App\Ad;
+use App\AdRegion;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class Region extends Controller
 {
-    //
+    public function page($country, $region)
+    {
+        $entity = AdRegion::where('slug', '=', $region)->first();
+
+        $results = Ad::getAds()->where('ad_cities.id', $entity->id)
+            ->paginate(15);
+
+        $ads = Ad::getLoopArray($results);
+
+        return view('front.ad.country')->with([
+            'entity' => $entity,
+            'ads' => $ads,
+            'links' => $results->links('front.widgets.paginate'),
+            'breadcrumbs' => 'region.page'
+        ]);
+    }
 }
