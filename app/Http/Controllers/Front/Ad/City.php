b27cb1467604f98808056ede0e05e5924ee671e5
vagrant <vagrant@homestead>
2019-11-13 23:05:05 +0000

    Натянута страница для городов

--- app/Http/Controllers/Front/Ad/City.php
+++ app/Http/Controllers/Front/Ad/City.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\Front\Ad;
 
+use App\Ad;
+use App\AdCity;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class City extends Controller
 {
-    //
+    public function page($country, $region, $city)
+    {
+        $entity = AdCity::where('slug', '=', $city)->first();
+
+        $results = Ad::getAds()->where('ad_cities.id', $entity->id)
+            ->paginate(15);
+
+        $ads = Ad::getLoopArray($results);
+
+        return view('front.ad.country')->with([
+            'entity' => $entity,
+            'ads' => $ads,
+            'links' => $results->links('front.widgets.paginate'),
+            'breadcrumbs' => 'city.page'
+        ]);
+    }
 }
