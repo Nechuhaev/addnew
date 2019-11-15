0027e1fc4e0d4764f8bac53010ea92fe2f936090
vagrant <vagrant@homestead>
2019-11-15 22:08:50 +0000

    Кеширование страницы городов

--- app/Http/Controllers/Front/Ad/Country.php
+++ app/Http/Controllers/Front/Ad/Country.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use Debugbar;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
@@ -17,45 +18,50 @@ class Country extends Controller
     public function getList()
     {
 
-        $data['countries'] = [];
-
-        $countries = AdCountry::all();
 
 
-        foreach ($countries as $country) {
-            $results = DB::table('ad_cities')
-                ->select('ad_cities.slug', 'ad_regions.slug AS region_slug', 'ad_cities.name', DB::raw("COUNT(ads.id) as ads_count"))
-                //->select('ad_cities.slug', 'ad_cities.name', DB::raw("1 as ads_count"))
-                ->leftJoin('ad_regions', 'ad_cities.region_id', '=', 'ad_regions.id')
-                ->leftJoin('ad_countries', 'ad_regions.country_id', '=', 'ad_countries.id')
-                ->leftJoin('ads', 'ads.city_id', '=', 'ad_cities.id')
-                ->where('ad_countries.id', $country->id)
-                ->orderBy('ads_count', 'desc')
-                ->groupBy("ad_cities.id")
-                ->limit('10')
-                ->get();
+        $data['countries'] = Cache::remember('countries_info', 50000, function () {
+            $countries = AdCountry::all();
 
-            $cities = [];
-            foreach ($results as $result) {
+            $countries_array = [];
+            foreach ($countries as $country) {
+                $results = DB::table('ad_cities')
+                    ->select('ad_cities.slug', 'ad_regions.slug AS region_slug', 'ad_cities.name', DB::raw("COUNT(ads.id) as ads_count"))
+                    //->select('ad_cities.slug', 'ad_cities.name', DB::raw("1 as ads_count"))
+                    ->leftJoin('ad_regions', 'ad_cities.region_id', '=', 'ad_regions.id')
+                    ->leftJoin('ad_countries', 'ad_regions.country_id', '=', 'ad_countries.id')
+                    ->leftJoin('ads', 'ads.city_id', '=', 'ad_cities.id')
+                    ->where('ad_countries.id', $country->id)
+                    ->orderBy('ads_count', 'desc')
+                    ->groupBy("ad_cities.id")
+                    ->limit('10')
+                    ->get();
 
-                $path = $country->slug . '/' . $result->slug;
-                $cities[] = [
-                    'url' => route('city.page', [
-                        'country' => $country->slug,
-                        'region' => $result->region_slug,
-                        'city' => $result->slug]),
-                    'name' => $result->name,
-                    'ads_count' => $result->ads_count,
+                $cities = [];
+                foreach ($results as $result) {
+
+                    $path = $country->slug . '/' . $result->slug;
+                    $cities[] = [
+                        'url' => route('city.page', [
+                            'country' => $country->slug,
+                            'region' => $result->region_slug,
+                            'city' => $result->slug]),
+                        'name' => $result->name,
+                        'ads_count' => $result->ads_count,
+                    ];
+                }
+
+                $countries_array[] = [
+                    'url' => route('country.page', ['country' => $country->slug]),
+                    'name' => $country->name,
+                    'image' => $country->image,
+                    'cities' => $cities
                 ];
             }
 
-            $data['countries'][] = [
-                'url' => route('country.page', ['country' => $country->slug]),
-                'name' => $country->name,
-                'image' => $country->image,
-                'cities' => $cities
-            ];
-        }
+            return $countries_array;
+        });
+
 
 
 
