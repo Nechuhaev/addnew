f25d9a252765a40d12ab8dc342d509c7aeaf4df8
vagrant <vagrant@homestead>
2019-12-20 09:14:15 +0200

    Tag manager inserted

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -16,6 +16,10 @@
     @yield('style')
 </head>
 <body class=""> <!-- fixed -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M89V7L"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class="debugGrid">
     <div>
         <div>
