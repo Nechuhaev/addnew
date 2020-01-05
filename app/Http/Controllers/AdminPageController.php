65792330721b104c0e1c37c4d9b058fe52091ad2
vagrant <vagrant@homestead>
2020-01-05 17:28:52 +0200

    Добавлено количество объявлений по странам/категориям

--- app/Http/Controllers/AdminPageController.php
+++ app/Http/Controllers/AdminPageController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Ad;
+use App\AdCountry;
 use App\ArticleCategory;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class AdminPageController extends Controller
 {
@@ -23,6 +25,20 @@ class AdminPageController extends Controller
         $data['ads_count'] = Ad::count();
         $data['ads_count_today'] = Ad::whereDate('created_at', Carbon::today())->count();
         $data['ads_count_week'] = Ad::whereDate('created_at', '>', Carbon::today()->subDays(30))->count();
+
+        $data['top_countries'] = DB::table('ad_countries')
+            ->selectRaw('ad_countries.name, COUNT(ads.id) as ads_count, MAX(ads.created_at) as last_created_at')
+            ->leftJoin('ad_regions', 'ad_regions.country_id', '=', 'ad_countries.id')
+            ->leftJoin('ad_cities', 'ad_cities.region_id', '=', 'ad_regions.id')
+            ->leftJoin('ads', 'ads.city_id', '=', 'ad_cities.id')
+            ->groupBy('ad_countries.name')->orderBy('ads_count', 'desc')->limit(10)->get();
+
+
+        $data['top_categories'] = DB::table('ad_categories')
+            ->selectRaw('ad_categories.name, COUNT(ads.id) as ads_count, MAX(ads.created_at) as last_created_at')
+            ->leftJoin('ads', 'ads.category_id', '=', 'ad_categories.id')
+            ->groupBy('ad_categories.name')->orderBy('ads_count', 'desc')->limit(10)->get();
+
         return view('admin.index')->with($data);
     }
 
