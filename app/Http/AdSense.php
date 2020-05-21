ce43b1b59d8bf3c89e24e6cf2b93adeef031a61f
Anatolii Kozyura <anatolii.koziura@gmail.com>
2020-05-21 23:24:36 +0300

    Ads check

--- app/Http/AdSense.php
+++ app/Http/AdSense.php
@@ -8,7 +8,8 @@ class AdSense {
             echo '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>';
             switch ($position) {
                 case 'top':
-                    echo '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1409649052535600" data-ad-slot="8755878337" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+                    //echo '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1409649052535600" data-ad-slot="8755878337" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+                    echo '<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-1409649052535600" data-ad-slot="8755878337"></ins>';
                     break;
                 case 'home-vertical':
                     echo '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1409649052535600" data-ad-slot="7184113110" data-ad-format="auto" data-full-width-responsive="true"></ins>';
