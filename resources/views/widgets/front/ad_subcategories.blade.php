78abefc99b5e4146878c9bfbef662dbb205b142e
Anatolii Koziura <anatolii.koziura@gmail.com>
2020-12-08 21:17:10 +0000

    Переработан виджет категорий

--- resources/views/widgets/front/ad_subcategories.blade.php
+++ resources/views/widgets/front/ad_subcategories.blade.php
@@ -1,9 +1,9 @@
-<h1 class="category-title">
+<h2 class="category-title">
     <img src="/{{ $config['parent']->image }}" alt="{{ $config['parent']->name }}" class="category-img">
     <span>{{ $config['parent']->name }}</span>
-</h1>
+</h2>
 <ul class="category-submenu">
     @foreach($config['categories'] as $category)
-        <li><a href="{{ $category['url'] }}">{{ $category['name'] }}</a></li>
+        <li class="{{ $category['active'] ? 'active' : null  }}"><a href="{{ $category['url'] }}">{{ $category['name'] }}</a></li>
     @endforeach
 </ul>
