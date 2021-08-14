422e151a84f307c70555bf32ba765b685eb1e2a5
ololower <anatolii.koziura@gmail.com>
2021-08-14 14:04:11 +0300

    somethig was changed

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -186,7 +186,7 @@
 </header>
 
 <div style="padding: 48px;margin-top: 100px;background: #000; color: #fff; text-align: center; font-size: 16px">
-    addnew.biz
+    http://addnew.biz
 </div>
 @yield('content')
 
