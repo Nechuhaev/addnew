08116abe4329f806601b2a8f397588f182dcee4e
Гасанбек Джарбаев <gasand92@yandex.ru>
2021-09-03 16:14:26 +0300

    add cities in search

--- app/Http/Controllers/API/Ad/Item.php
+++ app/Http/Controllers/API/Ad/Item.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\Ad;
 
 use App\Ad;
+use App\AdCity;
 use App\AdCategory;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -14,10 +15,16 @@ class Item extends Controller
     public function autocomplete($name = null) {
 
         if ($name) {
+            $cities = AdCity::where('name', 'like', "%$name%")->orderBy('name', 'asc')
+                ->where('region_id', 642)
+                ->take(10)
+                ->get();
+
             $items = Ad::where('name', 'like', "%$name%")->orderBy('name', 'asc')
                 ->take(10)
                 ->get();
         } else {
+            $cities = null;
             $items = Ad::orderBy('region_id', 'desc')
                 ->orderBy('name', 'asc')
                 ->take(10)
@@ -29,13 +36,18 @@ class Item extends Controller
             if ($category->parent_id != '0') {
                 $category = AdCategory::find($category->parent_id);
             }
-            
             $items[$i]->image = '/'.$category->image;
+        }
+        
+        if ($cities) {
+            foreach ($items as $attr => $value) {
+                $cities[] = $value;
+            }
         }
 
         if ($items) {
             //return CityResourse::collection($cities);
-            return response()->json($items);
+            return response()->json($cities);
         } else {
             return response()->json(['error' => 'Объявлений не найдено']);
         }
