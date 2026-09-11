82704a8954f1da4567f54c505fc19b02555e76ee
Oleksandr Nechukhaiev <43204291+Nechuhaev@users.noreply.github.com>
2026-09-11 09:40:42 +0300

    Add 'info_uk' to fillable attributes in User model

--- app/User.php
+++ app/User.php
@@ -63,7 +63,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'firstname', 'email', 'password', 'is_shop_owner', 'site_url', 'country', 'banner',
-        'telephone', 'info', 'image',
+        'telephone', 'info', 'info_uk', 'image',
     ];
 
     /**
