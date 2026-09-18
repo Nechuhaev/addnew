bfb4f7682c00e0bc065cff2c4b0880ba93dd83df
Oleksandr Nechukhaiev <43204291+Nechuhaev@users.noreply.github.com>
2026-09-18 08:24:46 +0300

    Change bodyText rendering to allow HTML content

--- resources/views/mail/shop-admin-message.blade.php
+++ resources/views/mail/shop-admin-message.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
     <div style="max-width: 600px; margin: 0 auto; padding: 20px;">
-        <div style="white-space: pre-wrap;">{{ $bodyText }}</div>
+        <div>{!! $bodyText !!}</div>
         <hr style="margin-top: 30px; border: none; border-top: 1px solid #eee;">
         <p style="font-size: 12px; color: #999;">
             Це повідомлення надіслано адміністрацією дошки оголошень addnew.biz.
