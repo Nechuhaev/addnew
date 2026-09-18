2efc62458105ab76ded8b58f61d4d5111d914d9d
Oleksandr Nechukhaiev <43204291+Nechuhaev@users.noreply.github.com>
2026-09-18 10:33:41 +0300

    Fix textarea to allow empty body input

--- resources/views/admin/shops/edit.blade.php
+++ resources/views/admin/shops/edit.blade.php
@@ -151,7 +151,7 @@
                         </div>
                         <div class="form-group">
                             <label for="body">Текст листа</label>
-                            <textarea name="body" id="body" class="form-control content" rows="10" required></textarea>
+                            <textarea name="body" id="body" class="form-control content" rows="10"></textarea>
                         </div>
                         <button type="submit" class="btn btn-success">Надіслати</button>
                     </form>
